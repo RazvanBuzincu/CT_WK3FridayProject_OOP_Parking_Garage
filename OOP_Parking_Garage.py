3bf4ed67582dd3455405ec13ab8b10ac3d7cef21
RazvanBuzincu <razvanbuzincu@gmail.com>
2024-01-19 13:35:03 -0600

    second commit with small adjustments

--- OOP_Parking_Garage.py
+++ OOP_Parking_Garage.py
@@ -4,30 +4,49 @@ class ParkingGarage():
         self.total_tickets_available = total_tickets_available
         self.parking_spaces_available = parking_spaces_available
         self.current_ticket = {}
+        self.user_response = None  # Assuming user_response is a class attribute
 
 
-
+    # Justin
     def take_ticket(self):
         # self.total_tickets_available = self.parking_spaces_available  ?
         while True:
             if self.parking_spaces_available >= 1:
                 print("Welcome to our Garage!")
-                user_response = input("Lucky for you, we currently have space available. Would you like a ticket? Yes or No").title()
-                if user_response == "Yes":
+                self.user_response = input("Lucky for you, we currently have space available. Would you like a ticket? Yes or No").title()
+                if self.user_response == "Yes":
                     print("Okay that'll be $20.00")
-                elif user_response == "No":
+                elif self.user_response == "No":
                     print("Okay have a nice day.")
                 else:
                     print("Sorry that is an invalid option.")
-
-
 
             else:
                 print("Sorry we're currently full. Please come back later.")
                 break
 
+
+    # Razvan
     def pay_for_parking(self):
+        if self.user_response == "Yes"
+           self.current_ticket = +1
 
 
 
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # william
     def leave_garage(self):
